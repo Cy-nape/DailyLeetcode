1b93507efd0602c32379eb185d1306a11724c9fa
Cy-nape <152891875+Cy-nape@users.noreply.github.com>
2025-05-02 19:08:05 +0530

    Time: 0 ms (100%), Space: 19.4 MB (86.63%) - LeetHub

--- LeetCode/Easy/0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ LeetCode/Easy/0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -10,32 +10,17 @@
  */
 class Solution {
 public:
-    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-    ListNode* dummy = new ListNode(0);
-    ListNode* tail = dummy;
-
-    while (list1 && list2) {
-        if (list1->val <= list2->val) {
-            tail->next = new ListNode(list1->val);
-            list1 = list1->next;
-        } else {
-            tail->next = new ListNode(list2->val);
-            list2 = list2->next;
+    ListNode* mergeTwoLists(ListNode* head1, ListNode* head2) {
+        if(head1==NULL || head2==NULL){
+            return head1 == NULL ? head2 : head1;
         }
-        tail = tail->next;
-    }
-    while (list1) {
-        tail->next = new ListNode(list1->val);
-        list1 = list1->next;
-        tail = tail->next;
-    }
 
-    while (list2) {
-        tail->next = new ListNode(list2->val);
-        list2 = list2->next;
-        tail = tail->next;
-    }
-
-    return dummy->next; 
+        if(head1->val <= head2->val){
+            head1->next = mergeTwoLists(head1->next,head2);
+            return head1;
+        }else{
+            head2->next = mergeTwoLists(head1,head2->next);
+            return head2;
+        }
     }
 };
