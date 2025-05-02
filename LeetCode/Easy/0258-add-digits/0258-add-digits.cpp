55994ec9d5295c7b57c8d0ea37a900a8ee64afb1
Cy-nape <152891875+Cy-nape@users.noreply.github.com>
2025-05-02 18:04:19 +0530

    Time: 0 ms (100%), Space: 8.5 MB (98.18%) - LeetHub

---
+++ LeetCode/Easy/0258-add-digits/0258-add-digits.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int addDigits(int num) {
+        int sum =0;
+        while(num>0){
+            sum+= num%10;
+            num /=10;
+        }
+
+        if(sum<10){
+            return sum;
+        }else{
+            num = sum;
+            return addDigits(num);
+        }
+
+        return sum;
+    }
+};
