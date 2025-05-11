70c2b85d67ac549852511791d50b189cd6be9b04
Cy-nape <152891875+Cy-nape@users.noreply.github.com>
2025-05-11 15:24:48 +0530

    Time: 0 ms (100%), Space: 7.9 MB (39.74%) - LeetHub

--- LeetCode/Easy/1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.cpp
+++ LeetCode/Easy/1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.cpp
@@ -1,12 +1,6 @@
 class Solution {
 public:
     int countOdds(int low, int high) {
-        int cnt = 0;
-        for(int i = low; i<=high ; i++){
-            if(i%2!=0){
-                cnt++;
-            }
-        }
-        return cnt;    
-    }
+    return (high - low) / 2 + (low % 2 != 0 || high % 2 != 0 ? 1 : 0);
+}
 };
