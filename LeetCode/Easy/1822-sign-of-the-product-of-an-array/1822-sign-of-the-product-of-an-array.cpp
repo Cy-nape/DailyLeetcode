c826748853cd70e4a660a57749183faa6659afa2
Cy-nape <152891875+Cy-nape@users.noreply.github.com>
2025-05-12 23:17:13 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ LeetCode/Easy/1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int arraySign(vector<int>& nums) {
+        _INT128 pro = 1;
+        for(int i =0; i<nums.size();i++){
+            pro *= nums[i];
+        }
+        if(pro>0){
+            return 1;
+        }
+        else if(pro<0){
+            return -1;
+        }else{
+            return 0;
+        }
+    }
+};
