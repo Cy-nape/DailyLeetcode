7d2a89ae542492519f536b0a95dc8f67d1a76cb0
Cy-nape <152891875+Cy-nape@users.noreply.github.com>
2025-05-17 22:45:36 +0530

    Time: 49 ms (42.06%), Space: 90.8 MB (31.26%) - LeetHub

--- LeetCode/Easy/0217-contains-duplicate/0217-contains-duplicate.cpp
+++ LeetCode/Easy/0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        unordered_set<int> numSet;
-        for (int num : nums) {
-            if (numSet.find(num) != numSet.end()) {
-                return true;
-            }
-            numSet.insert(num);
-        }
+        unordered_map<int, int> freqMap; 
+        for (int num : nums) { 
+            if(freqMap[num]==1){ 
+                return true; 
+            } 
+            freqMap[num]++; 
+        } 
         return false;
 
     }
