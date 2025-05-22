e3981d6bbbecfa03504efb5ae715d43823784617
Cy-nape <152891875+Cy-nape@users.noreply.github.com>
2025-05-22 13:47:44 +0530

    Time: 0 ms (100%), Space: 13.5 MB (97.87%) - LeetHub

---
+++ LeetCode/Easy/0035-search-insert-position/0035-search-insert-position.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& arr, int target) {
+        int lo = 0, hi = arr.size() - 1;
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (arr[mid] == target) {
+                return mid;
+            } else if (arr[mid] < target) {
+                lo = mid + 1;
+            } else {
+                hi = mid - 1;
+            }
+        }
+        return lo;
+    }
+};
